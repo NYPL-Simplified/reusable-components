5c33c91f0e7663babd0999d97b47bd245f1aa8d7
Adriana Alter <adrianaalter@nypl.org>
2019-01-03 14:27:41 -0500

    Still trying--removing src from dev branch

--- src/reducers/libraries.ts
+++
@@ -1,5 +0,0 @@
-import { LibrariesData } from "../interfaces";
-import ActionCreator from "../actions";
-import createFetchEditReducer from "./createFetchEditReducer";
-
-export default createFetchEditReducer<LibrariesData>(ActionCreator.GET_ALL_LIBRARIES);
