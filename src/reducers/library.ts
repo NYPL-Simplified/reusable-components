edecdfebaafe8ae3a950e965965e7a76db29004e
Adriana Alter <adrianaalter@nypl.org>
2019-01-03 14:18:45 -0500

    Removed src folder from master

--- src/reducers/library.ts
+++
@@ -1,8 +0,0 @@
-import { LibraryData } from "../interfaces";
-import ActionCreator from "../actions";
-import createFetchEditReducer from "./createFetchEditReducer";
-
-export default createFetchEditReducer<LibraryData>(
-  ActionCreator.GET_ONE_LIBRARY,
-  ActionCreator.EDIT_STAGES
-);
