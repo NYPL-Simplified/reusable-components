1d8f85b7a38a7c86ec8f1ce653075c9b7c718333
Adriana Alter <adrianaalter@nypl.org>
2019-01-24 16:03:50 -0500

    Arrow navigation for tabs (tests to follow)

--- src/components/reusables/Tabs.tsx
+++ src/components/reusables/Tabs.tsx
@@ -18,9 +18,31 @@ export default class Tabs extends React.Component<TabsProps, TabsState> {
     this.state = { tab: Object.keys(this.props.items)[0] };
   }
 
-  select(e: __React.SyntheticEvent) {
+  select(e:  __React.KeyboardEvent & __React.MouseEvent) {
     let tabName = (e.currentTarget as HTMLElement).innerText;
-    this.setState({ tab: tabName });
+    if (e.keyCode) {
+      // Keyboard navigation with arrow keys
+      let tabs = Object.keys(this.props.items);
+      let idx = tabs.indexOf(tabName);
+      let newIdx: number;
+      if (e.keyCode === 39) {
+        // Right arrow key: go to the next tab, or go back to the beginning if you were already on the last tab
+        newIdx = idx < tabs.length - 1 ? idx + 1 : 0;
+      }
+      else if (e.keyCode === 37) {
+        // Left arrow key: go to the previous tab, or go to the end if you were already on the first tab
+        newIdx = idx === 0 ? tabs.length - 1 : idx - 1;
+      }
+      else {
+        return;
+      }
+      this.setState({ tab: tabs[newIdx] });
+      (this.refs[newIdx] as HTMLElement).focus();
+    }
+    else {
+      // Click
+      this.setState({ tab: tabName });
+    }
   }
 
   makeTabs(): Array<Array<JSX.Element>> {
@@ -39,8 +61,10 @@ export default class Tabs extends React.Component<TabsProps, TabsState> {
             className="btn btn-default"
             id={`tab-${idx}`}
             onClick={this.select}
+            onKeyDown={this.select}
             role="tab"
             tabIndex={current ? 0 : -1}
+            ref={`${idx}`}
           >
             {name}
           </button>
