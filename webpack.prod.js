baaf55e16288b590265f78f1d2e7ddb1dc192abc
Edwin Guzman <edwin.gzmn@gmail.com>
2019-04-05 12:34:59 -0400

    Adding source maps to the prod build.

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: "production",
 
-  // devtool: "source-map",
+  devtool: "source-map",
 
   entry: {
     app: [
