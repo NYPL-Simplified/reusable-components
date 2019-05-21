ae95911e8d37c91750dfde1b60f462660b1ce97d
Adriana Alter <adrianaalter@nypl.org>
2019-05-21 10:34:03 -0400

    Add new button styles to Storybook

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -20,14 +20,89 @@ const button = storiesOf('Components/Button', module)
       />,
       <Button
         callback={action('clicked')}
+        content="Success"
+        className="success"
+      />,
+      <Button
+        callback={action('clicked')}
         content="Inverted"
         className="inverted"
+      />,
+      <Button
+        callback={action('clicked')}
+        content="Squared"
+        className="squared"
+      />,
+      <Button
+        callback={action('clicked')}
+        content="Bottom-Squared"
+        className="bottom-squared"
+      />,
+      <Button
+        callback={action('clicked')}
+        content="Small"
+        className="small"
+      />,
+      <Button
+        callback={action('clicked')}
+        content="Big"
+        className="big"
+      />,
+      <Button
+        callback={action('clicked')}
+        content="Transparent"
+        className="transparent"
       />,
       <Button
         callback={action('clicked')}
         content="Disabled"
         disabled={true}
       />
+    ];
+  })
+  .add('with different placement', () => {
+    return [
+      <div className="button-container">
+        <Button
+          callback={action('clicked')}
+          content="Left-Align"
+          className="left-align"
+        />
+        <Button
+          callback={action('clicked')}
+          content="Right-Align"
+          className="right-align"
+        />
+      </div>,
+      <div className="button-container">
+        <Button
+          callback={action('clicked')}
+          content="Top-Align"
+          className="top-align"
+        />
+        <Button
+          callback={action('clicked')}
+          content="Bottom-Align"
+          className="bottom-align"
+        />
+      </div>,
+      <div className="button-container">
+        <Button
+          callback={action('clicked')}
+          content="Navbar"
+          className="navbar-btn"
+        />
+        <Button
+          callback={action('clicked')}
+          content="Inline"
+          className="inline"
+        />
+        <Button
+          callback={action('clicked')}
+          content="Centered"
+          className="centered"
+        />
+      </div>
     ];
   })
   .add('with icon', () => {
