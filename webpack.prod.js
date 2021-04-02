9357eaa8edcc38921a4c35d8cc1acfc97f3548ea
adriana-alter <adrianaalter@nypl.org>
2021-04-02 14:04:40 -0400

    updates to run npm publish

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { merge } = require('webpack-merge');
+const merge = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 
